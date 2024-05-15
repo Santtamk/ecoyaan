bdc44fde510d657a4079d718f66d2c53e75204ce
Santam Kumai <santtamk@gmail.com>
2024-05-15 21:17:20 +0530

    main section completed

--- src/homepage/HomePage.jsx
+++ src/homepage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import TopNav from './TopNav'
 import './css/nav.css'
 import HeroSection from './HeroSection'
+import MainSection from './MainSection'
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
           <TopNav />
           <HeroSection />
       </div>
+      <MainSection />
     </>
   )
 }
--- src/homepage/MainSection.jsx
+++ src/homepage/MainSection.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import paper_bag from '../images/paper_bag.jpg';
+import data from '../images/home.json';
+import './css/hero.css';
 
 function MainSection() {
   return (
-    <div>
+    <div className='d-flex flex-wrap justify-content-start align-items-start container p-4'>
         
+        <div class="card-deck">
+            {data.map((item, index) => {
+                return (
+                    <div class="card shadow-lg p-3 mb-5 bg-white rounded" key={index}>
+                        <img class="card-img-top image" src={paper_bag} alt="Card"/>
+                        <div class="card-body">
+                            <h4 class="font-weight-bold">{item.heading}</h4>
+                            <p class="card-text">{item.paragraph}</p>
+                        </div>
+                        <div class="card-footer">
+                        <small class="text-muted"></small>
+                        </div>
+                    </div>
+                )
+            })}
+            </div>
     </div>
   )
 }
